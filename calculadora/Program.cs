82dea940e5dbfbf08862aa621cb261cd9e26e620
GiovanaVari <giovana.vari.de.assis@gmail.com>
2023-05-10 15:36:53 -0300

    Calculadora e Hello

---
+++ calculadora/Program.cs
@@ -0,0 +1,12 @@
+﻿using calculadora.Models;
+Calculadora calc = new Calculadora();
+
+calc.Multiplicar(10, 3);
+calc.Dividir(10, 2);
+calc.Somar(10, 30);
+calc.Subtrair(10, 5);
+calc.Potencia(3, 3);
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
+calc.RaizQuadrada(9);
